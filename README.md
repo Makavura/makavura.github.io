7b595c6f21a8c175fa44176852607abadf4013b4
Makavura <makavuramughanga@gmail.com>
2024-04-30 16:23:04 +0300

    chore: dir scaffolding; loading ui; prev content deletion

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # makavura.github.io
 My Developer Portfolio
+
+# Color Scheme
+rgb(255, 166, 41) - #ffa629
+rgb(56, 182, 255) - #38b6ff
---
+++ build/add-styles.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addStyles = void 0;
+const addStyles = (el, styles) => Object.assign(el.style, styles);
+exports.addStyles = addStyles;
