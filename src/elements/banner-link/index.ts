579c1250bb86cc3d33fe99a65f39ef80ac498b87
Makavura <makavuramughanga@gmail.com>
2024-05-02 15:05:09 +0300

    ui: landing desktop

--- src/elements/banner-link/index.ts
+++ src/elements/banner-link/index.ts
@@ -6,15 +6,15 @@ class BannerLink extends HTMLElement {
   };
 
   connectedCallback() {
-    this.innerHTML = `<div class="px-1 py-1 bg-[#233DFF] rounded-md ">
-                <div class="drop-shadow-xl shadow-2xl  bg-white  p-2 rounded-md flex flex-row-reverse justify-between ">
-                    <div class="grid grid-cols-2 items-center place-content-between">
-                        <p class="aloja text-slate-400 text-2xl inline">
+    this.innerHTML = `<div class="px-1 pt-1 pb-4 bg-[#233DFF] rounded-md ">
+                <div class="drop-shadow-xl shadow-2xl  bg-white rounded-md flex flex-row-reverse  justify-end">
+                    <div class="grid grid-rows-2 w-full">
+                        <p class="aloja text-slate-500 text-4xl justify-self-end  self-start -ml-24 mt-4 inline z-10 drop-shadow-xl">
                         ${this.getAttribute(
                           "bannerPath"
                         )}
                         </p>
-                        <svg class="text-slate-700 place-self-end self-center" viewBox="0 0 16 16" height="1.5em" width="1.5em" {...props}>
+                        <svg class="text-slate-700 justify-self-end self-end p-2" viewBox="0 0 16 16" height="4em" width="4em" {...props}>
                             <path
                                 d="M14 0a2 2 0 012 2v12a2 2 0 01-2 2H2a2 2 0 01-2-2V2a2 2 0 012-2h12zM5.904 10.803L10 6.707v2.768a.5.5 0 001 0V5.5a.5.5 0 00-.5-.5H6.525a.5.5 0 100 1h2.768l-4.096 4.096a.5.5 0 00.707.707z" />
                         </svg>
@@ -23,7 +23,7 @@ class BannerLink extends HTMLElement {
                       this.hasAttribute("svgIcon")
                         ? `<img src="${this.getAttribute(
                             "svgIcon"
-                          )}" class="rounded-md h-48 drop-shadow-xl shadow-2xl mt-4" />`
+                          )}" class="rounded-md h-48 drop-shadow-xl shadow-2xl" />`
                         : null
                     }
                 </div>
